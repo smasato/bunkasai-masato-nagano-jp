be356797e29c94280d73ee6a7fcbafaa950deb8d
Administrator <sugiyama@masato.nagano.jp>
2015-06-27 12:44:21 +0900

    [modify]console.log削除
    js/topshare.jsのconsole.logが残ったままだったので削除

--- js/topshare.js
+++ js/topshare.js
@@ -3,8 +3,6 @@
      var title = document.title;
      var lineButtonUri = "http://line.me/msg/text/?" + encodeURI(title + " " )+ encodeURIComponent(url);
      var tweetButtonUri = "http://twitter.com/share?text=" + encodeURI(title) + "&" + "&&url=" + url;
-     console.log(lineButtonUri);
-     console.log(tweetButtonUri);
      $("div#share > a.line-button").attr('href', lineButtonUri);
      $("div#share > a.tweet-button").attr('href', tweetButtonUri);
  });
