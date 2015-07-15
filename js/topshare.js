5d53204813bed21a28fea8d9d91b283714f709ef
Administrator <sugiyama@masato.nagano.jp>
2015-07-15 21:42:06 +0900

    [modify]不要なファイルを削除
    不要なjs、cssを削除。htmlも整理。

--- js/topshare.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-    "use strict";
-    var url = location.href,
-        title = document.title,
-        lineButtonUri = "http://line.me/msg/text/?" + encodeURI(title + " ") + encodeURIComponent(url),
-        tweetButtonUri = "http://twitter.com/share?text=" + encodeURI(title) + "&" + "&&url=" + url;
-    $("div#share > a.line-button").attr('href', lineButtonUri);
-    $("div#share > a.tweet-button").attr('href', tweetButtonUri);
-});
